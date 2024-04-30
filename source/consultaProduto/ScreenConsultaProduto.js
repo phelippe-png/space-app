b80bbba23652aba24c07bad018b25fc643e9bb5b
Phelippe <phelippe.gabriel.7@gmail.com>
2024-04-30 13:48:29 -0300

    commit

--- source/consultaProduto/ScreenConsultaProduto.js
+++ source/consultaProduto/ScreenConsultaProduto.js
@@ -1,11 +1,12 @@
-import { StyleSheet, Text } from "react-native"
-import { Camera } from 'expo-camera'
+import { Dimensions, StyleSheet, Text } from "react-native"
+import { AutoFocus, Camera } from 'expo-camera'
 import { View } from "react-native"
 import { useState } from "react"
 import { BarCodeScanner } from "expo-barcode-scanner"
 
 const ScreenConsultaProduto = () => {
   const [ean, setEan] = useState()
+  const heightScreen = Dimensions.get('window').height
 
   const OnBarCodeScanned = (data) => {
     console.log(data.data)
@@ -23,7 +24,15 @@ const ScreenConsultaProduto = () => {
         }}
         onBarCodeScanned={OnBarCodeScanned}
         ratio="16:9"
+        autoFocus={AutoFocus.on}
       />
+
+      <View style={[
+        styles.barcodeMask, 
+        {
+          borderTopWidth: (heightScreen/2) - 40, borderBottomWidth: (heightScreen/2) - 40
+        }
+      ]}/>
     </View>
   )
 }
@@ -32,6 +41,18 @@ const styles = StyleSheet.create({
   camera: {
     width: '100%',
     height: '97%'
+  },
+
+  barcodeMask: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    borderLeftWidth: 30,
+    borderRightWidth: 30,
+    borderTopColor: 'rgba(52, 52, 53, 0.7)',
+    borderLeftColor: 'rgba(52, 52, 52, 0.7)',
+    borderRightColor: 'rgba(52, 52, 52, 0.7)',
+    borderBottomColor: 'rgba(52, 52, 52, 0.7)',
   }
 })
 
